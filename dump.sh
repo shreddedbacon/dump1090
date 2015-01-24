958c2a4ba9d2bbe47d55a78181ceb6e91292da43
Ben Jackson <b@benjackson.email>
2015-01-24 17:36:11 +1100

    updated start up script for screen

--- dump.sh
+++ dump.sh
@@ -1,12 +1,26 @@
-#!/bin/bash
-START=95
-INSTALLDIR=/home/pi/dump1090
+#!/bin/sh
 
-start() {
-    screen -S dump1090 -d -m -L $INSTALLDIR/dump1090 --net >> /dev/null
-}
+### BEGIN INIT INFO
+# Provides:          dump
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Example initscript
+# Description:       This service is used to manage a servo
+### END INIT INFO
 
-stop() {
-    screen -r dump1090 -X quit
-}
+DUMP1090=/home/pi/dump1090
+OPTS="--net"
 
+case "$1" in
+start)	cd $DUMP1090
+        screen -S dump1090 -d -m -L $DUMP1090/dump1090 $OPTS >> /dev/null
+	;;
+restart) $0 stop
+	$0 start
+	;;
+stop)	screen -r dump1090 -X quit
+	;;
+esac
+exit 0
